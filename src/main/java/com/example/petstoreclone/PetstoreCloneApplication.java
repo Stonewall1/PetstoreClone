8cfdb3b3615f356f45193ae21e84d5957c89a599
Stonewall1 <impression97p@gmail.com>
2022-10-29 20:36:00 +0300

    Initial commit

---
+++ src/main/java/com/example/petstoreclone/PetstoreCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.petstoreclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstoreCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetstoreCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petstoreclone/PetstoreCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petstoreclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstoreCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
